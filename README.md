35eaeb62230e0c50b0686f7fec556df752afa3b8
selfstudymaster <webselfmaster@gmail.com>
2022-05-07 15:50:33 +0900

    README修正

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # math-algorithm-book
 ## 『問題解決のための「アルゴリズム X 数学」が基礎からしっかり身につく本』独習用
 ### Javaのソースを利用しています。
+
+### 自動採点システム(プログラム問題)
+https://atcoder.jp/contests/math-and-algorithm
---
+++ main/java/Code_2_01_1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Code_2_01_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 入力部分
+        System.out.println(5 + N); // 出力部分
+    }
+}
