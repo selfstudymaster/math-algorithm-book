53a24dad1b861ecb1e4394cfab3a6089099d9788
selfstudymaster <webselfmaster@gmail.com>
2022-05-07 19:15:51 +0900

    Code_2_01_4

---
+++ main/java/Code_2_01_4.java
@@ -0,0 +1,26 @@
+// 10進法を2進法に変換するプログラム
+
+import java.util.*;
+
+public class Code_2_01_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 入力部分
+
+        String answer = "";
+
+        while (N >= 1) {
+            // N % 2 は N を 2 で割った余り（例：N = 13 の場合 1）
+            // N / 2 は N を 2 で割った値の整数部分（例：N = 13 の場合 6）
+            if (N % 2 == 0) {
+                answer = "0" + answer;
+            }
+            if (N % 2 == 1) {
+                answer = "1" + answer;
+            }
+            N = N / 2;
+        }
+
+        System.out.println(answer); // 出力部分
+    }
+}
