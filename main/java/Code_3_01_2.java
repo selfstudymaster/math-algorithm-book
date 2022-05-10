4301f7b760a801f0306a1a4fd1e658e9ee902a3d
selfstudymaster <webselfmaster@gmail.com>
2022-05-11 00:53:22 +0900

    Code_3_01_2新規作成、Code_3_01_1修正

--- main/java/Code_3_01_2.java
+++ main/java/Code_3_01_2.java
@@ -1,7 +1,7 @@
-// 素数判定を行うプログラム
+// 高速な素数判定を行うプログラム
 import java.util.Scanner;
 
-class Code_3_01_1 {
+class Code_3_01_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long N = sc.nextLong();
@@ -9,18 +9,15 @@ class Code_3_01_1 {
 
         if (answer == true) {
             System.out.println("prime");
-        }
-        else {
+        } else {
             System.out.println("not prime");
         }
     }
 
-    // 素数か素数でないかを判定するメソッド
     static boolean isPrime(long N) {
-        // 2以上の整数N に対し、N が素数であればtrue、N が素数でなければfalseを戻す
-        for (long i = 2; i <= N - 1; i++) {
+        // 2 以上の整数 N に対し、N が素数であれば true、素数でなければ false を戻す
+        for (long i = 2; i * i <= N; i++) {
             if (N % i == 0) {
-                // N が i で割り切れた場合、この時点で素数ではないと分かる
                 return false;
             }
         }
