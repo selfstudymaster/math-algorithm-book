6d5031dcb5f20a1bf5b0ecdb31760cea7fa9c732
selfstudymaster <webselfmaster@gmail.com>
2022-05-19 23:29:12 +0900

    Code_3_01_1 コメント修正

--- main/java/Code_3_01_1.java
+++ main/java/Code_3_01_1.java
@@ -18,12 +18,14 @@ class Code_3_01_1 {
     // 素数か素数でないかを判定するメソッド
     static boolean isPrime(long N) {
         // 2以上の整数N に対し、N が素数であればtrue、N が素数でなければfalseを戻す
-        for (long i = 2; i <= N - 1; i++) {
+        for (long i = 2; i <= N-1; i++) {
+            // Nを2からN-1までの値(この場合はi)で割った余りが0ならば、iで割り切れるので、Nは素数ではない
             if (N % i == 0) {
                 // N が i で割り切れた場合、この時点で素数ではないと分かる
                 return false;
             }
         }
+        // Nがiで割り切れない場合、Nは素数と分かる
         return true;
     }
 }
